05ab3d539a0087622db4c2d4178acf494f04978c
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-04-28 15:02:08 +0200

    inizializzato impostasto costatnti

--- script.js
+++ script.js
@@ -4,3 +4,13 @@
 //sconti
 //stampa prezzo 
 //stampa prezzo decimale
+const distanza = Number(prompt("inserire i km da percorrere previsti"));
+const eta = Number(prompt("inserire anni passeggero"));
+const prezzoperkm = 0.21;
+console.log(distanza, eta);
+
+const anziano = 65;
+const minorenne = 18;
+const scontoanziano = 0.4;
+const scontominorenne = 0.2;
+
